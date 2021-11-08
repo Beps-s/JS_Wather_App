a32c8c8ebc6353cb1712f8319e69f24d8bdcc4fd
UwUBeebi <chris.erman@khk.ee>
2021-11-08 19:24:05 +0200

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -16,3 +16,11 @@ function weatherDataFetch(city) {
 function cityWeather(e){
 	weatherDataFetch("Tartu");
 }
+
+function drawWeather(data){
+	var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+	var description = data.weather[0].description;
+	document.querySelector("#description").innerHTML=description;
+	document.querySelector("#temp").innerHTML=celcius+"&deg;";
+	document.querySelector("#location").innerHTML=data.name;
+}
